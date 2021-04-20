c1085d4b7cf0eb7f0400a3360be58e3405a1edc4
JerkGPF <1040316431@qq.com>
2021-04-20 19:37:29 +0800

    已完成部分 即时通讯 HR的个人中心 分享 即时通讯model TODO 热更新

--- app/src/main/java/com/gpfei/graduationproject/ui/activities/common/JobWebDetailsActivity.java
+++ app/src/main/java/com/gpfei/graduationproject/ui/activities/common/JobWebDetailsActivity.java
@@ -26,6 +26,7 @@ import com.gpfei.graduationproject.ui.activities.common.login.LoginAndRegisterAc
 import com.gpfei.graduationproject.utils.ToastUtils;
 import com.hyphenate.easeui.EaseConstant;
 import com.longsh.optionframelibrary.OptionCenterDialog;
+import com.mob.MobSDK;
 
 import java.util.ArrayList;
 
@@ -33,6 +34,7 @@ import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.QueryListener;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class JobWebDetailsActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView iv_back;
@@ -44,6 +46,8 @@ public class JobWebDetailsActivity extends AppCompatActivity implements View.OnC
     private Button btn_load;
     private TextView tv_collect;
     private String objectId;
+    String title;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +60,26 @@ public class JobWebDetailsActivity extends AppCompatActivity implements View.OnC
 
     private void initView() {
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //查找Person表里面id为6b6c11c537的数据
+                BmobQuery<DayBean> bmobQuery = new BmobQuery<DayBean>();
+                bmobQuery.getObject(objectId, new QueryListener<DayBean>() {
+                    @Override
+                    public void done(DayBean object,BmobException e) {
+                        if(e==null){
+                            title = object.getTitle_day();
+                            url = object.getUrl();
+                        }else{
+                        }
+                    }
+                });
+
+            }
+        }).start();
+
+        MobSDK.submitPolicyGrantResult(true, null);
         iv_back = findViewById(R.id.iv_back);
         tv_title = findViewById(R.id.tv_title);
         ll_collect = findViewById(R.id.ll_collect);
@@ -76,7 +100,7 @@ public class JobWebDetailsActivity extends AppCompatActivity implements View.OnC
         iv_sharing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ToastUtils.showImageToast(JobWebDetailsActivity.this, "分享成功");
+                share();
             }
         });
         ll_collect.setOnClickListener(new View.OnClickListener() {
@@ -87,37 +111,37 @@ public class JobWebDetailsActivity extends AppCompatActivity implements View.OnC
                     BmobQuery<DayBean> bmobQuery = new BmobQuery<DayBean>();
                     bmobQuery.getObject(objectId, new QueryListener<DayBean>() {
                         @Override
-                        public void done(DayBean object,BmobException e) {
-                            if(e==null){
+                        public void done(DayBean object, BmobException e) {
+                            if (e == null) {
                                 String authorId = object.getAuthor().getObjectId();
-                                new Thread(){
+                                new Thread() {
                                     @Override
                                     public void run() {
                                         //查找Person表里面id为6b6c11c537的数据
                                         BmobQuery<User> bmobQuery = new BmobQuery<User>();
                                         bmobQuery.getObject(authorId, new QueryListener<User>() {
                                             @Override
-                                            public void done(User object,BmobException e) {
-                                                if(e==null){
+                                            public void done(User object, BmobException e) {
+                                                if (e == null) {
                                                     String author = object.getUsername();
-                                                   runOnUiThread(new Runnable() {
-                                                       @Override
-                                                       public void run() {
-                                                           Log.d("author>>>>>>>>", author);
-                                                           Intent chat = new Intent(JobWebDetailsActivity.this,MessageActivity.class);
-                                                           chat.putExtra(EaseConstant.EXTRA_USER_ID,author);  //对方账号
-                                                           chat.putExtra(EaseConstant.EXTRA_CHAT_TYPE, EaseConstant.CHATTYPE_SINGLE); //单聊模式
-                                                           startActivity(chat);
-                                                       }
-                                                   });
-                                                }else{
-                                                    Toast.makeText(JobWebDetailsActivity.this, "查询失败"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                    runOnUiThread(new Runnable() {
+                                                        @Override
+                                                        public void run() {
+                                                            Log.d("author>>>>>>>>", author);
+                                                            Intent chat = new Intent(JobWebDetailsActivity.this, MessageActivity.class);
+                                                            chat.putExtra(EaseConstant.EXTRA_USER_ID, author);  //对方账号
+                                                            chat.putExtra(EaseConstant.EXTRA_CHAT_TYPE, EaseConstant.CHATTYPE_SINGLE); //单聊模式
+                                                            startActivity(chat);
+                                                        }
+                                                    });
+                                                } else {
+                                                    Toast.makeText(JobWebDetailsActivity.this, "查询失败" + e.getMessage(), Toast.LENGTH_SHORT).show();
                                                 }
                                             }
                                         });
                                     }
                                 }.start();
-                            }else{
+                            } else {
                                 Toast.makeText(JobWebDetailsActivity.this, "查找失败", Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -131,9 +155,24 @@ public class JobWebDetailsActivity extends AppCompatActivity implements View.OnC
 
     }
 
+    private void share() {
+        OnekeyShare oks = new OnekeyShare();
+        // title标题，微信、QQ和QQ空间等平台使用
+        //oks.setTitle(getString(R.string.share));
+        // titleUrl QQ和QQ空间跳转链接
+        oks.setTitleUrl(url);
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText(title);
+        // setImageUrl是网络图片的url
+        //oks.setImageUrl("https://hmls.hfbank.com.cn/hfapp-api/9.png");
+        // url在微信、Facebook等平台中使用
+        oks.setUrl(url);
+        // 启动分享GUI
+        oks.show(MobSDK.getContext());
+    }
+
     private void loadWeb() {
         String url = (String) getIntent().getExtras().get("url");
-
         if (url != null) {
             mWebView.loadUrl(url);
 //            new Thread() {
@@ -177,14 +216,14 @@ public class JobWebDetailsActivity extends AppCompatActivity implements View.OnC
         BmobQuery<SelectAndResume> bmobQuery = new BmobQuery<SelectAndResume>();
         bmobQuery.getObject(objectId, new QueryListener<SelectAndResume>() {
             @Override
-            public void done(SelectAndResume object,BmobException e) {
-                if(e==null){
-                    if (object.getCollect()){
+            public void done(SelectAndResume object, BmobException e) {
+                if (e == null) {
+                    if (object.getCollect()) {
                         tv_collect.setText("已收藏");
-                    }else {
+                    } else {
                         tv_collect.setText("收藏");
                     }
-                }else{
+                } else {
                     Toast.makeText(JobWebDetailsActivity.this, "查询失败", Toast.LENGTH_SHORT).show();
                 }
             }
